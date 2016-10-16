07a777ced1629ff1f5697bb7646874f36ec26f90
rooa <hiroakihayash88@gmail.com>
2016-10-16 16:09:22 -0400

    Functionalize image dataset preprocessing. :ca:

--- src/runexp.py
+++ src/runexp.py
@@ -10,11 +10,26 @@ from gridopts import *
 from models import *
 from dna import DNAMonitor
 
+
+def pre_process_image(args):
+    (X_train, y_train), _ = DATASET_INFO[args.dataset]["loader"]()
+    if args.n_samples is not None:
+        X_train, y_train = X_train[:args.n_samples], y_train[:args.n_samples]
+    X_train = X_train.astype("float32") / 255.
+    if X_train.ndim == 3:
+        X_train = X_train[:, np.newaxis, :, :]
+    if args.flatten:
+        X_train = X_train.reshape((X_train.shape[0], -1))
+    y_train = np_utils.to_categorical(y_train, DATASET_INFO[args.dataset]["nb_classes"])
+    return X_train, y_train
+
+
 DATASET_INFO = {
-    "mnist": {"loader": mnist.load_data, "nb_classes": 10},
-    "cifar10": {"loader": cifar10.load_data, "nb_classes": 10},
-    "cifar100": {"loader": cifar100.load_data, "nb_classes": 100}
+    "mnist": {"loader": mnist.load_data, "nb_classes": 10, "preprocess": pre_process_image},
+    "cifar10": {"loader": cifar10.load_data, "nb_classes": 10, "preprocess": pre_process_image},
+    "cifar100": {"loader": cifar100.load_data, "nb_classes": 100, "preprocess": pre_process_image}
 }
+
 
 arg_parser = ArgumentParser()
 arg_parser.add_argument("--optimizer", type=str, required=True, choices=OPTIMIZERS_INDEX.keys())
@@ -30,15 +45,7 @@ args = arg_parser.parse_args()
 
 grid_opt = OPTIMIZERS_INDEX[args.optimizer](**args.opt_args)
 
-(X_train, y_train), _ = DATASET_INFO[args.dataset]["loader"]()
-if args.n_samples is not None:
-    X_train, y_train = X_train[:args.n_samples], y_train[:args.n_samples]
-X_train = X_train.astype("float32") / 255.
-if X_train.ndim == 3:
-    X_train = X_train[:, np.newaxis, :, :]
-if args.flatten:
-    X_train = X_train.reshape((X_train.shape[0], -1))
-y_train = np_utils.to_categorical(y_train, DATASET_INFO[args.dataset]["nb_classes"])
+X_train, y_train = DATASET_INFO[args.dataset]["preprocess"](args)
 
 best_final_loss = np.inf
 for opt in grid_opt:
